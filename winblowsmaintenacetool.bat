15439f979425865ed56bfca1e4c0b129cc0fa26b
Alexander <57069715+Oxygemo@users.noreply.github.com>
2020-02-08 13:06:46 +0100

    Oops, something broke
    revert to automatic

--- winblowsmaintenacetool.bat
+++ winblowsmaintenacetool.bat
@@ -1,42 +1,42 @@
 @echo off
 color 1f
-title Winblows Maintenace Tool vWho cares about the version number but it's 0.1 alpha by Odyssey346
+title Winblows Maintenace Tool vWho cares about the version number but it's 0.1rev1 alpha by Odyssey346
 echo  --------------------------------------
 echo  / Winblows Maintenace Tool v0.1 alpha \
 echo  / by Odyssey346                       \
 echo  / Enjoy                               \
 echo  / Discord: Odyssey346#9848            \
+echo  / Revision 1                          \
 echo  --------------------------------------
 echo I hope you have remembered to open the tool with Administrative permissions.
-echo List of commands:
-echo 1. Starts the frickin' tool
-echo 2. Credits!
-echo 3. Some nice ASCII art
-set /p input= 
-if %input%==1 goto Tool
-if %input%==2 goto Credits
-if %input%==3 goto ASCII
+ping 127.0.0.1 >nul
 
-:Tool
     echo Doing System File Check. this checks if your windows doesnt have missing files/corrupted system files.
     color 01
     sfc /scannow
     echo Done. 
     echo Tool is going to clean up your Temp folder now. Results will not be shown, due to the long list of files being deleted.
+    ping 127.0.0.1 >nul
     del /q/f/s %TEMP%\* >nul
+    cls 
     echo We have cleaned your Temp folder now. Now it's free of sh*t you dont need!
+    ping 127.0.0.1 >nul 
     echo Note: Some things might not have been removed. This is OK, as some apps are currently using it. You can maunally delete them anyway. Some we dont have access to.
+    echo We're going to open up Disk Cleanup. DO NOT SELECT DOWNLOADS. Unselect it if it's selected. It's for your own good.
+    ping 127.0.0.1 >nul
+    cleanmgr
+    echo It has been opened. 
     echo Everything has been checked. If you want the source code, right click the bat file and edit it.
+    ping 127.0.0.1 >nul
     echo Press any key to exit.
     pause >nul
+    goto Credits
 :Credits
     cls
     echo Winblows Maintenace Tool
-    echo Version 0.1 alpha
+    echo Version 0.1 rev1 alpha
     echo By Odyssey346
     echo Contributors:
     echo Odyssey346 for the base code
     echo Press any key to exit  the program.
     pause >nul
-:ASCII
-start "" https://youtube.com/watch?v=dQw4w9WgXcQ
