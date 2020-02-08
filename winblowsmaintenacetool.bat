74282c0e4119fe5ae64472f81119d1066d105131
Alexander <57069715+Oxygemo@users.noreply.github.com>
2020-02-08 16:05:52 +0100

    Added back admin check

--- winblowsmaintenacetool.bat
+++ winblowsmaintenacetool.bat
@@ -1,15 +1,32 @@
 @echo off
 color 1f
-title Winblows Maintenace Tool vWho cares about the version number but it's 0.1rev1 alpha by Odyssey346
+title Winblows Maintenace Tool vWho cares about the version number but it's 0.1rev1.1 alpha by Odyssey346
 echo  --------------------------------------
 echo  / Winblows Maintenace Tool v0.1 alpha \
 echo  / by Odyssey346                       \
 echo  / Enjoy                               \
 echo  / Discord: Odyssey346#9848            \
-echo  / Revision 1                          \
+echo  / Revision 1.1                        \
 echo  --------------------------------------
 echo I hope you have remembered to open the tool with Administrative permissions.
 ping 127.0.0.1 >nul
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+        echo The program will proceed. Press any key.
+    ) else (
+        echo Failure: Current permissions aren't Administrative. Restart with Administrative permissions.
+        echo The program will not proceed.
+    )
+
+    pause >nul
+
+
 
     echo Doing System File Check. this checks if your windows doesnt have missing files/corrupted system files.
     color 01
@@ -34,7 +51,7 @@ ping 127.0.0.1 >nul
 :Credits
     cls
     echo Winblows Maintenace Tool
-    echo Version 0.1 rev1 alpha
+    echo Version 0.1 rev1.1 alpha
     echo By Odyssey346
     echo Contributors:
     echo Odyssey346 for the base code
